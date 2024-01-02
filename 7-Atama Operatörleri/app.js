644a19fdee7ccb803d23693a947fd688b3eece20
Damla Çakır <damlacakr61@gmail.com>
2024-01-02 12:51:57 +0300

    Bölüm 12

---
+++ 7-Atama Operatörleri/app.js
@@ -0,0 +1,41 @@
+//------ ATAMA OPERATÖRLERİ -----
+
+/*
+
+= : atama
+== : eşittir.
+===
++=
+*=
+/=
+%=
+**=
+
+*/
+
+// let a = 6
+// let a = 6
+//? let b = 7
+
+// let sonuc = a * b
+// console.log(sonuc == 11);
+
+//? console.log(3=="3");
+// içindeki değerler eşitse true yapıyor
+
+//? === : hep tipleri hem de değerlerinn eşit olup olmadığına bakar
+
+// console.log(3==="3");
+
+//? += *= /= %= **=
+
+// let sayi = 4;
+// sayi = sayi+2;
+
+//  //kolay yolu:
+//  sayi +=2;
+//  console.log(sayi)
+
+// aynı mantık diğerleri de
+ 
+
