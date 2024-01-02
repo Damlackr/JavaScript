6d0de2afe1c390d9495711d8e73412e775d72899
Damla Çakır <damlacakr61@gmail.com>
2024-01-02 13:37:03 +0300

    Bölüm 15

---
+++ 9-Dialog Kutuları/app.js
@@ -0,0 +1,30 @@
+/*
+------Diyalog Kutuları------
+-Alert
+-Prompt
+-Confirm
+
+
+*/
+
+// window içerisinde tanımlanmıştır. tek yazılarak çağrılabilir.
+
+
+//alert("Nasılsın..") // popup açıyor-bilgilendirici
+
+// değer alma : Prompt 
+
+// let isim = prompt("isminizi giriniz:");
+// console.log(isim);
+
+//kullanıcılardan alınan değeler her zaman strin döner.
+
+// Türk dönüşümleri ile stringi int yapabiliriz daha sonra
+
+
+//? Confirm : onaylama
+
+let sonuc = confirm("silmek istediğinize emin misiniz");
+ console.log(sonuc);
+
+ // değeri boolean döner - true ya da false
