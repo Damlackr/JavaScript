c0a7a7eadf3de49be9b5789219e7c0931f94ce56
Damla Çakır <damlacakr61@gmail.com>
2024-01-02 12:40:45 +0300

    Bölüm 11

---
+++ 6-Aritmetik Operatörler/app.js
@@ -0,0 +1,63 @@
+//  Aritmetik Operatörler
+
+/* --------> 
+= atama operatörü önemli !
++
+-
+*
+/
+%
+++
+--
+**
+
+
+*/
+// //? Toplama
+// let not1 = 30;
+// let not2 = 70;
+
+// console.log(not1 + not2);
+
+//? Çıkarma
+// let not1 = 30;
+// let not2 = 70;
+
+// console.log(not2 - not1);
+
+// //? Çarpma
+
+// let not1 = 30;
+// let not2 = 70;
+
+// console.log(not1 * not2);
+
+//! değişken ismi yazılırken türkçe karakter kullanılmamalı,
+//! birleşik olarak yazılır ikincisinin baş harfi büyük yazılır.
+
+// ? Bölme
+
+// let not1 = 130;
+// let not2 = 10;
+
+// console.log((not1*not2)/2);
+
+// ? Mod almma
+
+// console.log(10%3);
+// console.log(12%5);
+
+//? ++ : 1 arttırmak için kullanılır
+
+// let a = 10;
+// a++;
+// a++;
+// a++;
+// a--;
+
+// console.log(a);
+
+//? **: kuvvet almak için
+
+// console.log(5**3);
+// console.log(2**2);
