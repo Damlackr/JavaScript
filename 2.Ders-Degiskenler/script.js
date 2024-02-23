439fefcb9843a7e9ed3597716729239073fc3a61
Damla Çakır <damlacakr61@gmail.com>
2024-02-23 16:49:59 +0300

    değişken adı yazma kuralları

--- 2.Ders-Degiskenler/script.js
+++ 2.Ders-Degiskenler/script.js
@@ -4,3 +4,9 @@ var zam = 0.35
 
 console.log (maasAli + (maasAli * zam)) // maaş ali
 console.log (maasCan + (maasCan * zam)) // maaş can
+
+
+// YAZMA KURALLARI:
+//Türkçe karakter kullanmamalıyız.
+//Arada boşluk olmaz.
+// Sayı ile bailayamaz.
