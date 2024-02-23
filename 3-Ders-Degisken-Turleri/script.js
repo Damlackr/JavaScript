d8cd6cb7a8754c33ea5a21995c8c29ed61549c2a
Damla Çakır <damlacakr61@gmail.com>
2024-02-23 17:11:06 +0300

    toString()

--- 3-Ders-Degisken-Turleri/script.js
+++ 3-Ders-Degisken-Turleri/script.js
@@ -4,8 +4,18 @@ let urunFiyati = 15000; //number
 console.log(typeof urunAdi);
 console.log(typeof urunFiyati);
 
-let sayi1 = "10";
-let sayi2 = "20";
+//let sayi1 = "10";
+//let sayi2 = "20";
 
 
-console.log(Number(sayi1) + Number(sayi2));
+//console.log(Number(sayi1) + Number(sayi2));
+let sayi1 = 10;
+let sayi2 = 20;
+
+
+console.log(sayi1.toString()+ sayi2.toString());
+
+let isim = "Sena";
+let soyad = "turan";
+
+console.log(isim + " " + soyad);
