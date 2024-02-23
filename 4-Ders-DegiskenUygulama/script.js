6c3fd42f2a4734cc0bf0f9a3cb6c3ee73423e3f8
Damla Çakır <damlacakr61@gmail.com>
2024-02-23 17:29:09 +0300

    uygulama sorusu

---
+++ 4-Ders-DegiskenUygulama/script.js
@@ -0,0 +1,17 @@
+/*
+1- iki öğrencinin aşağıdaki bilgilerini değişkenler içerisinde saklayınız.
+
+        Öğrenci 1:
+            İsim    : ada bilgi
+            Doğum Tarihi: 2012
+            matematik notları: 70, 70,80
+
+        Öğrenci 1:
+            İsim    : yiğit bilgi
+            Doğum Tarihi: 2010
+            matematik notları: 40, 40,70
+    
+2- Öğrencilerin yaş bilgilerini değişkende tutunuz.
+3- Öğrencilerinin ders ortalama notunu değişkende saklayınız.
+4- Öğrencilerin 50 geçme notuna göre başarı durumlarını değişkende saklayınız.
+*/
