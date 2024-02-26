f6cb6a17de4827d61403753b5f0238acddaa4aca
Damla Çakır <damlacakr61@gmail.com>
2024-02-26 11:43:44 +0300

    koşul

---
+++ 6-Ders-IfElse-Kosul/script.js
@@ -0,0 +1,39 @@
+console.log("merhaba");
+console.log("merhaba");
+console.log("merhaba");
+
+if (3>2)
+{
+    console.log("çalışır");
+}
+
+
+let username = "damlacakir";
+let password = "1234";
+
+let isLoggedin = (username == "damlacakir");
+
+if(isLoggedin){
+    if ( password == "1234")
+    {
+    console.log("uygulamaya giriş yapıldı.");
+   }
+
+}
+else{
+    console.log("uygulamaya giriş başarısız.");
+}
+
+//let isLoggedin = true;
+
+//if (isLoggedin)
+//{
+   // console.log("uygulamaya giriş yapıldı");
+//}
+//else{
+   // console.log("merhaba");
+//}
+//if (!isLoggedin)
+//{
+    //console.log("merhaba");
+//}
