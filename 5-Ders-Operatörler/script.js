a84d651c33cda6f0b63eafd1606bbafa9ba9bdd7
Damla Çakır <damlacakr61@gmail.com>
2024-02-26 11:28:04 +0300

    operatörler

---
+++ 5-Ders-Operatörler/script.js
@@ -0,0 +1,33 @@
+let sonuc;
+let a = 10, b = 20; c = 32;
+
+
+// 1- Aritmatik Operatörler
+sonuc = a + b;
+sonuc = a - b;
+sonuc = a * b; 
+sonuc = a / b;
+sonuc = c % a;
+sonuc = a++;  // a + 1;
+sonuc = ++a;  // a + 1;
+sonuc = --a;
+sonuc = a--;
+// 2- Atama Operatörleri
+sonuc = a;
+sonuc += a // sonuc = sonuc + a
+sonuc -= a // sonuc = sonuc - a
+sonuc *= a // sonuc = sonuc * a
+sonuc /= a // sonuc = sonuc / a
+sonuc %= a // sonuc = sonuc % a
+// 3- Karşılaştırma Operatörleri
+
+sonuc = (a==b)
+sonuc = (a!=b)
+sonuc = (3==="3") // Tip ve değer kontrolü yapar.
+sonuc = (a>b)
+sonuc = (a<b)
+sonuc = (a <=b)
+
+// 4- Mantıksal Operatörler
+
+console.log(sonuc);
