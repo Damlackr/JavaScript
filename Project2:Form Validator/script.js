325e7986cdcdc98b4ea8164880bed31394a3ca5a
Damla Çakır <damlacakr61@gmail.com>
2024-03-06 18:58:33 +0300

    kodların düzeltilmesi

--- Project2:Form Validator/script.js
+++ Project2:Form Validator/script.js
@@ -18,51 +18,68 @@ function succes(input){
 }
 
 
-const validateEmail = (email) => {
-  return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
+function checkEmail(input){
+  const re= /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+   if(re.test(input.value)){
+        succes(input);
+   }else{
+        error(input,'hatalı bir mail adresi');
+   }
 };
+
+function checkRequired(/*input*/ inputs){
+    //console.log(input);
+    inputs.forEach(function(input) { 
+        if(input.value === ''){
+        error(input, `${input.id} is required.`);
+    }else{
+        succes(input);
+    }
+});
+}
 
 form.addEventListener('submit',function(e){
     e.preventDefault(); // var sayılan özellik kapanıyor form subbit etmiyor.consolden silinmiyor
-    // console.log(username.value);
+    // // console.log(username.value);
 
-    if(username.value === ''){
-        //username.className = 'form-control error_'
-        //username.className = 'form-control is-invalid' // error ile aynı işlemi yapmaktadır
-        error(username,'username gerekli');
+    // if(username.value === ''){
+    //     //username.className = 'form-control error_'
+    //     //username.className = 'form-control is-invalid' // error ile aynı işlemi yapmaktadır
+    //     error(username,'username gerekli');
 
-    }else{
-        succes(username);
-        //username.className = 'form-control is-valid'
-    }
-    //console.log(username.value );
-    if(email.value === ''){
-        //username.className = 'form-control error'
-        //email.className = 'form-control is-invalid' // error ile aynı işlemi yapmaktadır
-        error(email,'email gerekli');
+    // }else{
+    //     succes(username);
+    //     //username.className = 'form-control is-valid'
+    // }
+    // //console.log(username.value );
+    // if(email.value === ''){
+    //     //username.className = 'form-control error'
+    //     //email.className = 'form-control is-invalid' // error ile aynı işlemi yapmaktadır
+    //     error(email,'email gerekli');
       
 
-    }else if(!validateEmail(email.value)){
-        error(email,'düzgün bir mail adresi giriniz')
-    }else{
-        succes(email);
-    }
-    if(password.value === ''){
-        //username.className = 'form-control error'
-        //password.className = 'form-control is-invalid' // error ile aynı işlemi yapmaktadır
-        error(password,'password gerekli');
-    }else{
-        succes(password);
-    }
-    if(repassword.value === ''){
-        //username.className = 'form-control error'
-        //repassword.className = 'form-control is-invalid' // error ile aynı işlemi yapmaktadır
-        error(repassword,'repassword gerekli');
-    }else{
-        succes(repassword);
-    }
+    // }else if(!validateEmail(email.value)){
+    //     error(email,'düzgün bir mail adresi giriniz')
+    // }else{
+    //     succes(email);
+    // }
+    // if(password.value === ''){
+    //     //username.className = 'form-control error'
+    //     //password.className = 'form-control is-invalid' // error ile aynı işlemi yapmaktadır
+    //     error(password,'password gerekli');
+    // }else{
+    //     succes(password);
+    // }
+    // if(repassword.value === ''){
+    //     //username.className = 'form-control error'
+    //     //repassword.className = 'form-control is-invalid' // error ile aynı işlemi yapmaktadır
+    //     error(repassword,'repassword gerekli');
+    // }else{
+    //     succes(repassword);
+    // }
+    checkRequired([username,email,password,repassword]);
+    //checkRequired(email);
+    //checkRequired(password);
+    //checkRequired(repassword);
+    checkEmail(email);
 });
