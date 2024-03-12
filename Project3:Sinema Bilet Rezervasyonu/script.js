995ee3074f178d7e1d3b8c337efa59766c1d8c54
Damla Çakır <damlacakr61@gmail.com>
2024-03-12 14:40:05 +0300

    bilet bilgilerinin local storage üzerinden alınması

--- Project3:Sinema Bilet Rezervasyonu/script.js
+++ Project3:Sinema Bilet Rezervasyonu/script.js
@@ -4,6 +4,9 @@ const amount = document.getElementById('amount');
 const select = document.getElementById('movie');
 const seats = document.querySelectorAll('.seat:not(.reserved');
 
+getFromLocalStorage();
+calculateTotal();
+
 container.addEventListener('click',function(e){
     if(e.target.classList.contains('seat') && !e.target.classList.contains('reserved')){
         e.target.classList.toggle('selected');
@@ -45,6 +48,27 @@ function calculateTotal(){
 
         saveToLocalStroage(selectedSeatIndexs);       
 }
+
+function getFromLocalStorage(){
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    
+    if(selectedSeats != null && selectedSeats.length > 0) {
+        seats.forEach(function(seat,index){
+            if(selectedSeats.indexOf(index) > -1 ){
+                seat.classList.add('selected');
+            }
+        });
+    }
+    
+    
+    
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if (selectedMovieIndex != null ){
+        select.selectedIndex = selectedMovieIndex;
+    }
+}
+
 
 function saveToLocalStroage(index){
     localStorage.setItem('selectedSeats',JSON.stringify(index));
