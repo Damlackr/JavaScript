c5e56fd0aa232f43a591ecba7e7781f695ca0259
Damla Çakır <damlacakr61@gmail.com>
2024-03-12 13:49:38 +0300

    local storagede seçilmiş filmlere göre koltukları görme

--- Project3:Sinema Bilet Rezervasyonu/script.js
+++ Project3:Sinema Bilet Rezervasyonu/script.js
@@ -2,6 +2,7 @@ const container = document.querySelector('.container');
 const count = document.getElementById('count');
 const amount = document.getElementById('amount');
 const select = document.getElementById('movie');
+const seats = document.querySelectorAll('.seat:not(.reserved');
 
 container.addEventListener('click',function(e){
     if(e.target.classList.contains('seat') && !e.target.classList.contains('reserved')){
@@ -15,9 +16,40 @@ select.addEventListener('change', function(e){
 })
 
 function calculateTotal(){
-        let selectedSeatCount = container.querySelectorAll('.seat.selected').length;
-        let price = select.value;
+        const selectedSeats = container.querySelectorAll('.seat.selected');
+        const selectedSeatArr = [];
+        const seatsArr = [];
+
+        selectedSeats.forEach(function(seat){
+            selectedSeatArr.push(seat);
+        });
+
+        //spread modern javada
+
+        seats.forEach(function(seat){
+            seatsArr.push(seat);
+        });
+
+
+        //[1,3,5]
+        let selectedSeatIndexs = selectedSeatArr.map(function(seat){
+            return seatsArr.indexOf(seat);
+        });
+
+        
+        
+        let selectedSeatCount = container.querySelectorAll('.seat.selected').length;    
         count.innerText = selectedSeatCount;
-        amount.innerText = selectedSeatCount * price;
+        amount.innerText = selectedSeatCount * select.value;
         //console.log(price);
+
+        saveToLocalStroage(selectedSeatIndexs);       
 }
+
+function saveToLocalStroage(index){
+    localStorage.setItem('selectedSeats',JSON.stringify(index));
+    localStorage.setItem('selectedMovieIndex', select.selectedIndex);
+}
+
+
+
